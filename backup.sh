aa0419007e79472bb8b019cabf1d5b6c128bfafc
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-05-11 11:28:51 -0400

    CHG snapshot and daily backups

--- backup.sh
+++ backup.sh
@@ -2,24 +2,26 @@
 
 cd $(dirname $(realpath $0))
 
+BACKUP_LIMIT=${1:-5}
+BACKUP_DIR=${2:-$(pwd)/.backups}
+TIMESTAMP="$(date +%s)"
 
-# if backup dir not defined set default
-if [ -z "${BACKUP_DIR}" ]; then
-    export BACKUP_DIR="$(pwd)/.data"
-fi
+echo "BACKUP_LIMIT=${BACKUP_LIMIT}"
+echo "BACKUP_DIR=${BACKUP_DIR}"
+echo "TIMESTAMP=${TIMESTAMP}"
 
 # create backup dir if necessary
 if [ ! -d ${BACKUP_DIR} ]; then
     mkdir -p ${BACKUP_DIR}
 fi
 
-# epoch time
-TIMESTAMP="$(date +%s)"
-
-if (( $(ls ${BACKUP_DIR} | wc -l) == 5 )); then
-    echo "deleting ${BACKUP_DIR}/$(ls ${BACKUP_DIR} | sort | head -n 1)"
-    rm -f ${BACKUP_DIR}/$(ls ${BACKUP_DIR} | sort | head -n 1)
+# Check the backup limit
+if (( $(ls ${BACKUP_DIR} | wc -l) == ${BACKUP_LIMIT} )); then
+    BACKUP_DEL="$(ls -tr ${BACKUP_DIR} | head -n 1)"
+    echo "deleting ${BACKUP_DIR}/${BACKUP_DEL}"
+    rm -f ${BACKUP_DIR}/${BACKUP_DEL}
 fi
 
+echo "---"
 echo "creating ${BACKUP_DIR}/${TIMESTAMP}.tar.gz"
 tar -czf ${BACKUP_DIR}/${TIMESTAMP}.tar.gz .data
